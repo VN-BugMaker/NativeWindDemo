1c54bd1c54c601e38441b88da406d191352a1553
VN-BugMaker <khoipn@proton.me>
2023-06-26 11:02:54 +0700

    Initial project with NativeWind

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App() {
+    return (
+     <View className="flex-1 items-center justify-center bg-white">
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+
+export default App;
